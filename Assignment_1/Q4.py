0321501c5fac6dc02fd115f128faedae4b7c1924
Ubuntu <ubuntu@ip-172-31-28-235.ec2.internal>
2016-02-16 10:20:17 +0000

    Updated Q4.py

--- Assignment_1/Q4.py
+++ Assignment_1/Q4.py
@@ -3,17 +3,34 @@ from yahoo_finance import Share
 import time
 import csv
 import os
+import numpy as np
 
-#subprocess.call(shlex.split('rm ./Q4_files/*.csv'))
+TIME_IN_MIN = 3 # min
+TIME_BETWEEN_ITERATIONS = 60 #sec
 
+def outlier (arr, number_of_std_devs):
+	sig = np.std(arr)
+	mu = np.mean(arr)
+	outlier_val= number_of_std_devs*sig
+	valid=[]
+	outlier = []
+	for elem in arr:
+		if (elem >= (mu - outlier_val)) and (elem <= (mu + outlier_val)):
+			valid.append(elem)
+		else:
+			outlier.append(elem)
+	return valid, outlier
 
-with open('Yahoo_symbols.csv','rb') as sym_list:
+def clear_files():
+         with open('Yahoo_symbols.csv','rb') as sym_list:
 		reader = csv.DictReader(sym_list)
 		for sym in reader:
 			PATH="/home/ubuntu/BigData/Assignment_1/Q4_files/"+sym["COMPANY"]+".csv"
 			os.remove(PATH)
 
-for i in range(0,4):
+
+def create_files():
+        for i in range(0,TIME_IN_MIN*60/TIME_BETWEEN_ITERATIONS):
 	    reader=csv.DictReader(open('Yahoo_symbols.csv','rb'))
      	    for sym in reader:
 
@@ -31,4 +48,21 @@ for i in range(0,4):
 			price = share_name.get_price()
 			writer = csv.DictWriter(file, fieldnames=["Time","Price"], delimiter=",", lineterminator="\n")
 			writer.writerow({"Time":timestamp ,"Price":price})		
-file.close()
+	    time.sleep(TIME_BETWEEN_ITERATIONS)	
+
+        file.close()
+
+def main():
+	clear_files()
+	create_files()
+
+
+def test_outlier():
+	a = np.random.randint(-1500, 1500, 2500).astype(np.float64)+np.random.rand(1,2500)
+	val,out= outlier(a,2)
+	print out
+
+test_outlier()
+
+### To do: 1. Update outlier Function
+### 	   2. Run code LIVE
