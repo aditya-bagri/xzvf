034cebe7c5a980c43295adb6e2ed27f370cbe10d
maanitmehra <maanitm@gmail.com>
2016-03-04 04:05:27 +0000

    Working Code

--- Assignment_2/alchemytest.py
+++ Assignment_2/alchemytest.py
@@ -1,19 +1,23 @@
 from alchemyapi import AlchemyAPI
 
-KEY = open('./api_key.txt','rb').read()[:-1]
+KEY = open('../../api_key.txt','rb').read()[:-1]
 
 
 def alcObj():
         return AlchemyAPI(KEY)
 
-obj = alcObj()
-test_text = "Hello"
 
-def sentAn(test_text):
-	response = obj.sentiment("text", test_text)
+def sentAn(obj, text):
+	response = obj.sentiment("text", text)
 	score 	 = response[u'docSentiment'][u'score']
 	type	 = response[u'docSentiment'][u'type']
 	return score, type
 
+def testAlc():
+	obj = alcObj()
+	test_text = "Donald Trump is a smurf."
+	score, type = sentAn(obj, test_text)
+	print "test_text:\t", test_text
+	print "Sentiment is %s %s" %(str(score), str(type))
 
-
+testAlc()
