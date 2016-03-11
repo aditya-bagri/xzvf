e4b2e8eb65d2bab187a9684651048182eae61bd2
maanitmehra <maanitm@gmail.com>
2016-03-11 13:23:16 +0000

    Updated Values.

--- Assignment_2/createModel.py
+++ Assignment_2/createModel.py
@@ -1,3 +1,19 @@
+###########################################################################
+### Algorithmic collaboration with 
+### Zoltan Onodi-Szucs, Palash Matey, Rikin Mathur
+### Aditya Bagri, Anubha Bhargava & Marshall Van Loon
+###
+###
+### Original Code Portions by Maanit Mehra
+###
+### References:
+### http://www.scientificpapers.org/wp-content/files/1134_How_to_use_Fibonacci_retracement_to_predict_forex_market.pdf
+###
+###
+### This code was completed as part of Assignment 2 of the
+### the Advanced Big Data class for the Spring 2016 Semester
+###########################################################################
+
 import csv
 import numpy as np
 import requests
@@ -10,13 +26,23 @@ import os
 import os.path
 
 
-INTERVAL = 60 #sec
-GOOGLE_QUOTE_URL = "http://www.google.com/finance/getprices?f=d,o,h,l,c,v&df=cpct&i="+str(INTERVAL)+"&q="
+from sklearn.linear_model import Ridge
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import make_pipeline
+
+from scipy.interpolate import interp1d
+
+INTERVAL = 1 #min
+GOOGLE_QUOTE_URL = "http://www.google.com/finance/getprices?f=d,o,h,l,c,v&df=cpct&i="+str(INTERVAL*60)+"&q="
 PATH = "./Det_DATA/"
 
+TIME_START = 1451606400
+
+
+
 ## This function collects detailed data from the Google Finance API
-## for each Ticker symbol. It saves this data in a file named after the company 
-## input. 
+## for each Ticker symbol. It saves this data in a file named after  
+## the company input. 
 def getHistory(symbol,company):
 	filename = PATH + company + ".csv"
 	try:
@@ -33,15 +59,50 @@ def getHistory(symbol,company):
 	resp = requests.get(stock_url)
 	resp_filter = resp.text.split(" ")
 	
-	## From 6 to exclude the textual information, upto -1 to exclude the last \n
+	## From 7 to exclude the textual information, upto -1 to exclude the final \n
 	text_list = resp_filter[0].split("\n")[7:-1]
 	file.write("TIME,CLOSE,HIGH,LOW,OPEN,VOLUME\n")
+	elem_list=[]
+	ctr = 0
 	for row in text_list: 
+		row_elems= row.split(',')
+		if row_elems[0] == u'30':
+			elem_list.append(row_elems) 
+			ctr = ctr + 1;
 		file.write(row+"\n")
-#	file.write(resp_filter)
 	file.close()	
+	return elem_list, ctr
 
+def cleanFile(symbol,company):
+	filename = PATH + company + ".csv"
+	file_cleaned = PATH + company +"_cleaned.csv"
+        try:
+                os.remove(file_cleaned)
+        except:
+                pass
+
+	reader=csv.DictReader(open(filename,'rb'))
+	time_list=[]
+    	for col in reader:
+        	time=col["TIME"]
+		
+		time_list.append(time)
+	stamp = 0
+	for time in time_list:	
+		if 'a' in time:
+			stamp= time[1:]
+			time = str( int(time[1:]) - TIME_START )
+		else:
+			time = str(int(stamp) + int(time) - TIME_START)
+
+def extrapol(x, y, x_new, degree):
+	f2 = interp1d(x, y, kind='cubic')
+	f = np.poly1d(np.polyfit(x, y, degree))
+	y_new = f(x_new)
+	return y_new
+	
 def main():
+	global var
         reader=csv.DictReader(open('Yahoo_symbols.csv','rb'))
         company_list=[]
         sym_list=[]
@@ -51,12 +112,23 @@ def main():
                 company_list.append(company)
                 sym_list.append(symbol)
         print sym_list
+	var = -6	
+	degree = 1
 
+	print "Degree:\t1\t2\t3\t4\t5"
         for i in range(0,len(sym_list)):
-#		print sym_list[i]
-                getHistory(sym_list[i], company_list[i])
-
-
+                elem_list, ctr = getHistory(sym_list[i], company_list[i])
+		ctr_arr = range(0,ctr)
+		clos_list = []
+		for elem in elem_list:
+			clos_list.append(float(elem[1]))
+		clos_arr = np.array(clos_list)
+		print "Ac:%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f"%(sym_list[i],extrapol(ctr_arr, clos_arr, ctr, 1),extrapol(ctr_arr, clos_arr, ctr, 2),extrapol(ctr_arr, clos_arr, ctr, 3), 
+		extrapol(ctr_arr, clos_arr, ctr, 4), extrapol(ctr_arr, clos_arr, ctr, 5))
+		ctr = ctr + var
+                print "Pr:%s\t%.2f\t%.2f\t%.2f\t%.2f\t%.2f\tACT:%.2f"%(sym_list[i],extrapol(ctr_arr, clos_arr, ctr, 1),extrapol(ctr_arr, clos_arr, ctr, 2),
+		extrapol(ctr_arr, clos_arr, ctr, 3), extrapol(ctr_arr, clos_arr, ctr, 4), extrapol(ctr_arr, clos_arr, ctr, 5), clos_arr[var])
+ 				
 main()
 
 
